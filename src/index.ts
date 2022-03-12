5f47ec0e7f649165adedae3875d4ab514c364d45
Vladyslav Sitalo <sitalov@amazon.com>
2022-03-11 17:22:25 -0800

    Update to use a new link structure

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,14 @@ import toConfigPageName from "roamjs-components/util/toConfigPageName"
 import runExtension from "roamjs-components/util/runExtension"
 import {createConfigObserver} from "roamjs-components/components/ConfigPage"
 import {watchTree} from "./watch"
-import {publishBlock} from "./publish"
+import {publishBlock, transclusionId} from "./publish"
 
 const ID = "transclude-me"
 const CONFIG = toConfigPageName(ID)
+const appUrl = `http://localhost:1234`
+// const appUrl = `https://transclude.me/`
+
+
 runExtension(ID, () => {
     createConfigObserver({title: CONFIG, config: {tabs: []}})
 
@@ -14,12 +18,14 @@ runExtension(ID, () => {
         .addCommand({
             label: 'Create a public transclusion of a block',
             callback: async () => {
-                console.log("hi")
                 const currentBlock = await window.roamAlphaAPI.ui.getFocusedBlock()
                 const currentBlockId = currentBlock["block-uid"]
 
+                //@ts-ignore
                 publishBlock(await window.roamAlphaAPI.data.pull("[*]", [":block/uid", currentBlockId]))
-                await navigator.clipboard.writeText(`http://localhost:1234/${currentBlockId}`)
+
+                await navigator.clipboard.writeText(`${appUrl}/roam/${await transclusionId(currentBlockId)}`)
+
                 watchTree(currentBlockId, (_, block) => {
                     publishBlock(block)
                 })
--- src/publish.ts
+++ src/publish.ts
@@ -1,8 +1,29 @@
 import {PullBlock} from "roamjs-components"
 import {setCloudState} from "@compose-run/client"
 
-export const publishBlock = (block: PullBlock) => {
-    const name = `transclude-me/roam/graph-name-todo/${block[":block/uid"]}`
-    console.log("publishing", name, block)
+export const publishBlock = async (block: PullBlock) => {
+    const name = `transclude-me/roam/${await transclusionId(block[":block/uid"])}`
+    console.log("publishing", getGraphName(), name, block)
     setCloudState({name: name, state: block})
+}
+
+const getGraphName = () => {
+    const currentUrl = new URL(window.location.href)
+    return currentUrl.hash.split('/')[2]
+}
+
+export const transclusionId = (currentBlockId: string) => sha256(`${getGraphName()}/${currentBlockId}`)
+
+async function sha256(message: string) {
+    // encode as UTF-8
+    const msgBuffer = new TextEncoder().encode(message)
+
+    // hash the message
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer)
+
+    // convert ArrayBuffer to Array
+    const hashArray = Array.from(new Uint8Array(hashBuffer))
+
+    // convert bytes to hex string
+    return hashArray.map(b => b.toString(16).padStart(2, '0')).join('')
 }
